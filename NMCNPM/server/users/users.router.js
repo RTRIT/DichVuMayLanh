cd531c60b4b510f5c3782fa7ccc37442c780044e
RTRIT <petertringuyen03@gmail.com>
2024-05-13 10:02:57 +0700

    adjust some html vs css

--- NMCNPM/server/users/users.router.js
+++ NMCNPM/server/users/users.router.js
@@ -7,12 +7,29 @@ const path = require('path');
 route.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../..', 'views', 'login.html'));
 });
+route.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '../..', 'views', 'index.html'));
+});
+route.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '../..', 'views', 'register.html'));
+});
+route.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, '../..', 'views', 'Admin.html'));
+});
+route.get('/Form', (req, res) => {
+    res.sendFile(path.join(__dirname, '../..', 'views', 'Form.html'));
+});
 
 route.get('/serviceList', (req, res) => {
     res.sendFile(path.join(__dirname, '../..', 'views', 'serviceList.html'));
 })
+route.get('/Form', (req, res) => {
+    res.sendFile(path.join(__dirname, '../..', 'views', 'Form.html'));
+});
 
-
+route.get('/Product', (req, res) => {
+    res.sendFile(path.join(__dirname, '../..', 'views', 'Product.html'));
+});
 
 
 
